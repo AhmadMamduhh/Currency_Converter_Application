5413f77b32128727a46011826c38d204564eaeb1
Ahmed Mamdouh <mamdu7x@gmail.com>
2020-07-29 19:35:57 +0200

    Finished the user interface design phase.

--- app/src/main/java/com/ahmedmamdouh/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/ahmedmamdouh/currencyconverter/MainActivity.kt
@@ -2,10 +2,32 @@ package com.ahmedmamdouh.currencyconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Setting up the currencies menus
+        setupSpinners()
+
+    }
+
+    private fun setupSpinners() {
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            applicationContext,
+            R.array.currencies_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            currencySpinner1.adapter = adapter
+            currencySpinner2.adapter = adapter
+        }
+        currencySpinner1.setSelection(1)
     }
 }
