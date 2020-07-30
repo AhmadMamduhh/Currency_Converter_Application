1ac1a3308babf06efb6ce4fb39e5eaec52464463
Ahmed Mamdouh <mamdu7x@gmail.com>
2020-07-30 14:20:29 +0200

    Added initial currency conversion logic to the application.

--- app/src/main/java/com/ahmedmamdouh/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/ahmedmamdouh/currencyconverter/MainActivity.kt
@@ -2,11 +2,14 @@ package com.ahmedmamdouh.currencyconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val MAX_CURRENCY_VALUE: Double = 999999.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +17,35 @@ class MainActivity : AppCompatActivity() {
 
         // Setting up the currencies menus
         setupSpinners()
+
+        // Conversion operation
+        convertButton.setOnClickListener {
+            when (valueEditText.text.toString()) {
+                "" -> Toast.makeText(this@MainActivity, R.string.empty_warning, Toast.LENGTH_LONG)
+                    .show()
+                else -> convertCurrency(valueEditText.text.toString())
+            }
+        }
+
+    }
+
+    private fun convertCurrency(currency: String) {
+        // This method is responsible for converting the currency and displaying the results
+        if (currencySpinner1.selectedItem == currencySpinner2.selectedItem)
+            Toast.makeText(this@MainActivity, R.string.same_currency_warning, Toast.LENGTH_LONG)
+                .show()
+        else {
+            outputCardView.visibility = View.VISIBLE
+            var convertedValue: Double = currency.toDouble()
+            when (currencySpinner2.selectedItem) {
+                "EGP" -> convertedValue /= 15.9
+                "USD" -> convertedValue *= 15.9
+            }
+            resultsText.text = "${convertedValue.toFloat()} ${currencySpinner2.selectedItem}"
+
+
+        }
+
 
     }
 
